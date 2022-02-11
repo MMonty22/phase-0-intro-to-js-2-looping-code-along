f2e6d1d6c5cb4f8a9dd9069926510e26bee22b76
Michael <michael.c.montgomery@gmail.com>
2022-02-11 17:45:08 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, string) {
+    const writtenCards = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${string} gift!`)
+        writtenCards.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`)
+    }
+    return writtenCards;
+}
+
+function countDown() {
+   let a = 10
+   while (a <= 10 && a >= 0) {
+       console.log(a--)
+   }
+   return a
+}
